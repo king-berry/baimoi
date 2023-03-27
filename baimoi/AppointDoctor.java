fede288c0a1f824048f0a045491c887856d06668
king-berry <111548775+king-berry@users.noreply.github.com>
2023-03-27 19:41:18 +0700

    dsa
    dsa

---
+++ baimoi/AppointDoctor.java
@@ -0,0 +1,22 @@
+package baimoi;
+import java.util.PriorityQueue;
+public class AppointDoctor {
+    public AppointDoctor(){
+    }
+
+    public static void main(String[] args) {
+        PriorityQueue pq = new PriorityQueue();
+        Doctor Cardiologist = new Doctor("Casuality", 1);
+        Doctor Gynecologist = new Doctor("Surgery", 2);
+        Doctor Paediatric = new Doctor("Routine Chek-up", 3);
+        pq.offer(Cardiologist);
+        pq.offer(Gynecologist);
+        pq.offer(Paediatric);
+        while (!pq.isEmpty()){
+            System.out.println("\nAppointing doctors bases on their prorities: ");
+            System.out.println("*********************************");
+            Doctor doctor = ((Doctor) pq.remove());
+            System.out.println(doctor.toString());
+        }
+    }
+}
